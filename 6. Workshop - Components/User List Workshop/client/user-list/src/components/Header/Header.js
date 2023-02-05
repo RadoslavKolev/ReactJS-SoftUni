6f6c3e50d5bfc9dbdfa3816616e2c074b618efc0
rkolev <radoslav.kolev@dxc.com>
2023-02-05 11:25:37 +0200

    Changed class attribute to className

--- 6. Workshop - Components/User List Workshop/client/user-list/src/components/Header/Header.js
+++ 6. Workshop - Components/User List Workshop/client/user-list/src/components/Header/Header.js
@@ -3,10 +3,10 @@ import styles from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header class={styles.header}>
-      <div class={styles.logo}>
-        <span class={styles.course}>React Course - June 2022</span>
-        <span class={styles.description}>User List Demo</span>
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <span className={styles.course}>React Course - June 2022</span>
+        <span className={styles.description}>User List Demo</span>
       </div>
     </header>
   );
