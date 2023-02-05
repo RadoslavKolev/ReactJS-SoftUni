3c67eba8b69f8888fdb61f83b0185c48f01f1f54
rkolev <radoslav.kolev@dxc.com>
2023-02-05 11:23:26 +0200

    Added Footer

--- 6. Workshop - Components/User List Workshop/client/user-list/src/App.js
+++ 6. Workshop - Components/User List Workshop/client/user-list/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 import './App.css';
 
@@ -7,6 +9,8 @@ const App = () => {
   return (
     <div>
       <Header />
+      
+      <Footer />
     </div>
   );
 };
---
+++ 6. Workshop - Components/User List Workshop/client/user-list/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer class={styles.footer}>
+      <p>Copyright &copy; designed by Mihail Valkov</p>
+    </footer>
+  );
+};
+
+export default Footer;
