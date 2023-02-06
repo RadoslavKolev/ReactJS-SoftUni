fab4f8c85c9ecafbbd1ecb782753fb248b1b2aea
rkolev <radoslav.kolev@dxc.com>
2023-02-06 14:03:23 +0200

    Image displays blank profile if profile img url is missing

--- 6. Workshop - Components/User List Workshop/client/user-list/src/components/UsersTable/UserRow/UserRow.js
+++ 6. Workshop - Components/User List Workshop/client/user-list/src/components/UsersTable/UserRow/UserRow.js
@@ -4,13 +4,14 @@ import { formatDate } from "../../../functions/formatDate";
 import UserActions from "./UserActions/UserActions";
 
 const UserRow = ({ user }) => {
+  const blankProfileUrl = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png";
   const formattedDate = formatDate(user.createdAt);
 
   return (
     <tr>
       <td>
         <img
-          src={user.imageUrl}
+          src={user.imageUrl || blankProfileUrl}
           alt={`${user.firstName}'s profile`}
           className="image"
         />
